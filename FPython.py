ea0a82a254954757e879c40bda6408fac7fbdcd9
Mark Webster <markwebster204@yahoo.co.uk>
2025-04-27 21:35:27 +0100

    Add enums for state and word type, needed once we allow word definition

--- FPython.py
+++ FPython.py
@@ -1,4 +1,5 @@
 import re
+from enum import Enum
 
 
 def tokenise(str):
@@ -25,49 +26,65 @@ def fp(x):
     return []
 
 
+class Word(Enum):
+    Base = 1
+    Compound = 2
+
+
+class State(Enum):
+    Execute = 1
+    Word = 2
+    Compile = 3
+
+
 class Forth:
     def __init__(self, silent=False):
         self.data = []
         if silent:
-            dot_word = (1, 0, lambda x: [])
+            dot_word = (1, 0, Word.Base, lambda x: [])
         else:
-            dot_word = (1, 0, fp)
+            dot_word = (1, 0, Word.Base, fp)
         self.words = {
-            "drop": (1, 0, lambda x: []),
+            "drop": (1, 0, Word.Base, lambda x: []),
             ".": dot_word,
-            "dup": (1, 2, lambda x: x + x),
-            "+": (2, 1, lambda x: [x[0] + x[1]]),
-            "-": (2, 1, lambda x: [x[0] - x[1]]),
-            "*": (2, 1, lambda x: [x[0] * x[1]]),
-            "/": (2, 1, lambda x: [x[0] // x[1]]),
-            "swap": (2, 2, lambda x: list(reversed(x))),
-            "over": (2, 3, lambda x: [x[0], x[1], x[0]]),
-            "tuck": (2, 3, lambda x: [x[1], x[0], x[1]]),
-            "rot": (3, 3, lambda x: [x[1], x[2], x[0]]),
-            "-rot": (3, 3, lambda x: [x[2], x[0], x[1]]),
+            "dup": (1, 2, Word.Base, lambda x: x + x),
+            "+": (2, 1, Word.Base, lambda x: [x[0] + x[1]]),
+            "-": (2, 1, Word.Base, lambda x: [x[0] - x[1]]),
+            "*": (2, 1, Word.Base, lambda x: [x[0] * x[1]]),
+            "/": (2, 1, Word.Base, lambda x: [x[0] // x[1]]),
+            "swap": (2, 2, Word.Base, lambda x: list(reversed(x))),
+            "over": (2, 3, Word.Base, lambda x: [x[0], x[1], x[0]]),
+            "tuck": (2, 3, Word.Base, lambda x: [x[1], x[0], x[1]]),
+            "rot": (3, 3, Word.Base, lambda x: [x[1], x[2], x[0]]),
+            "-rot": (3, 3, Word.Base, lambda x: [x[2], x[0], x[1]]),
         }
         self.silent = silent
+        self.state = State.Execute
 
     def do(self, str):
         for token in tokenise(str):
-            if token in self.words.keys():
-                lin, lout, word = self.words[token]
-                if (len(self.data) < lin):
-                    raise RuntimeError(
-                        "Data stack underflow: " + token)
-                used = self.data[-lin:]
-                rest = self.data[:-lin]
-                new = word(used)
-                if (len(new) != lout):
-                    raise RuntimeError(
-                        "Word output size error: " + token)
-                self.data = rest + new
-            else:
-                try:
-                    number = int(token)
-                except Exception:
-                    raise RuntimeError("Undefined word: " + token)
-                self.data.append(number)
+            match self.state:
+                case State.Execute:
+                    if token in self.words.keys():
+                        lin, lout, word_type, word = self.words[token]
+                        if (len(self.data) < lin):
+                            raise RuntimeError(
+                                "Data stack underflow: " + token)
+                        used = self.data[-lin:]
+                        rest = self.data[:-lin]
+                        new = word(used)
+                        if (len(new) != lout):
+                            raise RuntimeError(
+                                "Word output size error: " + token)
+                        self.data = rest + new
+                    else:
+                        try:
+                            number = int(token)
+                        except Exception:
+                            raise RuntimeError("Undefined word: " + token)
+                        self.data.append(number)
+                case _:
+                    raise RuntimeError("Not implemented")
         if not self.silent:
             print("ok")
         return
